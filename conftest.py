45f72f37d48e734823afc20d54beac6f16d5f02c
Sylvia-rnk <udinadiana97@gmail.com>
2025-11-21 13:24:43 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,23 @@
+from email.policy import default
+from pickletools import pybytes_or_str
+from sys import activate_stack_trampoline
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: es, fr, ru, etc.")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages':language})
+
+    print(f"\nStar browser with language: {language}")
+    browser = webdriver.Chrome(options=options)
+
+    yield browser
+    print("\nQuit browser..")
+    browser.quit()
